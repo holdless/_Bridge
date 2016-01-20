e72762afd901b8ea6f20942e00a35af223d417e6
Hiroshi Ch <holdless2@gmail.com>
2016-01-20 14:54:19 +0800

    Update c_call_java.cpp

--- Pos-tagging/Stanford/c_call_java.cpp
+++ Pos-tagging/Stanford/c_call_java.cpp
@@ -13,8 +13,10 @@
 #include <jni.h>
 using namespace std;
 
+jstring str2jstring(JNIEnv* env,const char* pat);
+std::string jstring2str(JNIEnv* env, jstring jstr);
+
 int _tmain(int argc, _TCHAR* argv[])
-//int main(int argc, char* argv[])
 {
     JavaVM *jvm;       /* denotes a Java VM */
     JNIEnv *env;       /* pointer to native method interface */
@@ -31,18 +33,63 @@ int _tmain(int argc, _TCHAR* argv[])
     delete options;
     /* invoke the Main.test method using the JNI */
     jclass cls = env->FindClass("stanford/pos/tagger/StanfordPosTagger");
-	// call "init" to initialize the stanford pos tagger model
+	
+    // call java method "init" to initialize the stanford pos tagger model
     jmethodID mid = env->GetStaticMethodID(cls, "init", "()Ledu/stanford/nlp/tagger/maxent/MaxentTagger;");
-//	jmethodID mid = env->GetStaticMethodID(cls, "main", "([Ljava/lang/String;)V");
     jobject tagger = env->CallStaticObjectMethod(cls, mid);
 
-	// call "tag" to pos-tag sentence
-    jmethodID mid2 = env->GetStaticMethodID(cls, "tag", "(Ledu/stanford/nlp/tagger/maxent/MaxentTagger;)I");
-    int a = env->CallStaticIntMethod(cls, mid2, tagger);
-	/* We are done. */
+    // call java method "tag" to pos-tag sentence
+    jmethodID mid2 = env->GetStaticMethodID(cls, "tag", "(Ledu/stanford/nlp/tagger/maxent/MaxentTagger;Ljava/lang/String;)I");
+    const char *sentence = "i am jack, how are you?";
+    // tranform c-string to java-string (since string in Java is an object)
+    jstring jsentence = str2jstring(env, sentence);
+    int a = env->CallStaticIntMethod(cls, mid2, tagger, jsentence);
+
     jvm->DestroyJavaVM();
-	
-	return 0;
+    return 0;
 }
 
+jstring str2jstring(JNIEnv* env,const char* pat)
+{
+    //Define java String class "strClass"
+    jclass strClass = (env)->FindClass("Ljava/lang/String;");
+    //get String(byte[],String) constructor to tranform local byte[] array to a new string.
+    jmethodID ctorID = (env)->GetMethodID(strClass, "<init>", "([BLjava/lang/String;)V");
+    //new a byte array
+    jbyteArray bytes = (env)->NewByteArray(strlen(pat));
+    //transform char* into byte array
+    (env)->SetByteArrayRegion(bytes, 0, strlen(pat), (jbyte*)pat);
+    // set String, preserve language class for the parameters of byte array transform to String
+    jstring encoding = (env)->NewStringUTF("GB2312"); 
+    //transform byte array into java String, then output
+    return (jstring)(env)->NewObject(strClass, ctorID, bytes, encoding);
+}
+ 
+ 
+std::string jstring2str(JNIEnv* env, jstring jstr)
+{   
+    char* rtn = NULL;   
+    jclass clsstring = env->FindClass("java/lang/String");   
+    jstring strencode = env->NewStringUTF("GB2312");   
+    jmethodID mid = env->GetMethodID(clsstring, "getBytes", "(Ljava/lang/String;)[B");   
+    jbyteArray barr = (jbyteArray)env->CallObjectMethod(jstr,mid,strencode);   
+    jsize alen = env->GetArrayLength(barr);   
+    jbyte* ba = env->GetByteArrayElements(barr,JNI_FALSE);   
+    if(alen > 0)   
+    {   
+        rtn = (char*)malloc(alen + 1);         
+        memcpy(rtn,ba,alen);   
+        rtn[alen] = 0;   
+    }   
+    env->ReleaseByteArrayElements(barr,ba,0);   
+    std::string stemp(rtn);
+    free(rtn);
+    return   stemp;   
+} 
 
+/*
+JNIEXPORT jstring JNICALL Java_JavaHowTo_sayHello(JNIEnv *env, jobject obj, string str) 
+{
+    return  env->NewStringUTF(str);
+}
+*/
