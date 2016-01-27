d0107c01cc5cdfc28ccd6569e76e6f30edfb96b7
Hiroshi Ch <holdless2@gmail.com>
2016-01-27 15:13:38 +0800

    Update c_embedding_py_separate.cpp

--- Pos-tagging/NLTK/c_embedding_py_separate.cpp
+++ Pos-tagging/NLTK/c_embedding_py_separate.cpp
@@ -16,72 +16,72 @@ PyObject* callPythonFunc(const char* scriptName,
 
 int main(int argc, char* argv[])
 {
-	// declare names of py script & functions
-	const char* script_name = "pos_tag";
-	const char* load_func_name = "loadPickle";
-	const char* tag_func_name = "posTagging";
-	const char* sentence = "i am jacjk";
+    // declare names of py script & functions
+    const char* script_name = "pos_tag";
+    const char* load_func_name = "loadPickle";
+    const char* tag_func_name = "posTagging";
+    const char* sentence = "i am jacjk";
 
-	// declare PyObject
-	PyObject *pTagger;
-	PyObject *pArgs, *pSentence;
+    // declare PyObject
+    PyObject *pTagger;
+    PyObject *pArgs, *pSentence;
 
-	// init C/Python interpreter
-	Py_SetProgramName(argv[0]);
+    // init C/Python interpreter
+    Py_SetProgramName(argv[0]);
     Py_Initialize();
     PySys_SetArgv(argc, argv);
 
-	// 1st call: loading tagger
+    // 1st call: loading tagger
     pTagger = callPythonFunc(script_name, 
-							 load_func_name,
-							 NULL);
+ 			     load_func_name,
+			     NULL);
 
-	// 2nd call: for pos-tagging
-	pArgs = PyTuple_New(2);
-	pSentence = PyString_FromString(sentence);
-	PyTuple_SetItem(pArgs, 0, pSentence);
-	PyTuple_SetItem(pArgs, 1, pTagger);
+    // 2nd call: for pos-tagging
+    pArgs = PyTuple_New(2);
+    pSentence = PyString_FromString(sentence);
+    PyTuple_SetItem(pArgs, 0, pSentence);
+    PyTuple_SetItem(pArgs, 1, pTagger);
     PyObject* b = callPythonFunc(script_name, 
-								 tag_func_name,
-								 pArgs);
+				 tag_func_name,
+				 pArgs);
 
     Py_DECREF(pArgs);
 //    Py_DECREF(pSentence); //<- after PyTuple_SetItem (who steals the refcnt), we don need to handle this!?
 //    Py_DECREF(pTagger);
     Py_DECREF(b);
 
-	Py_Finalize();
+    Py_Finalize();
 
     return 0;
 }
 
 
 PyObject* callPythonFunc(const char* scriptName, 
-					const char* funcName,
-					PyObject* pInpTuple)
+			 const char* funcName,
+			 PyObject* pInpTuple)
 {
-	PyObject *pName, *pModule, *pFunc, *pRetValue;
+    PyObject *pName, *pModule, *pFunc, *pRetValue;
 
     pName = PyString_FromString(scriptName);
     pModule = PyImport_Import(pName);
     Py_DECREF(pName);
 
-	if (pModule != NULL) 
-	{
+    if (pModule != NULL) 
+    {
         pFunc = PyObject_GetAttrString(pModule, funcName);
         // pFunc is a new reference
 
         if (pFunc && PyCallable_Check(pFunc)) 
-		{
+	{
             pRetValue = PyObject_CallObject(pFunc, pInpTuple);
 
             if (pRetValue != NULL) 
-			{
+	    {
                 printf("Call successful\n");
 //                Py_DECREF(pValue);
             }
             else 
-			{
+	    {
                 Py_DECREF(pFunc);
                 Py_DECREF(pModule);
                 PyErr_Print();
@@ -90,7 +90,7 @@ PyObject* callPythonFunc(const char* scriptName,
             }
         }
         else 
-		{
+	{
             if (PyErr_Occurred())
                 PyErr_Print();
             fprintf(stderr, "Cannot find function \"%s\"\n", funcName);
@@ -99,12 +99,12 @@ PyObject* callPythonFunc(const char* scriptName,
         Py_DECREF(pModule);
     }
     else 
-	{
+    {
         PyErr_Print();
         fprintf(stderr, "Failed to load \"%s\"\n", scriptName);
         return NULL;
     }
 
-	return pRetValue;
+    return pRetValue;
 }
 
