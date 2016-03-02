80ae44b07bcd2f86520b893d9ca5794cab4294c3
Hiroshi Ch <holdless2@gmail.com>
2016-03-02 10:14:34 +0800

    Create alphabetTable.py

---
+++ alphabetTable.py
@@ -0,0 +1,75 @@
+voiceInput = 'alpha'
+
+if voiceInput == 'alfa' || voiceInput == 'alpha':
+    ch = 'a';
+elif voiceInput == 'bravo':
+    ch = 'b';
+elif voiceInput == 'charlie':
+    ch = 'c';
+elif voiceInput == 'delta':
+    ch = 'd';
+elif voiceInput == 'echo':
+    ch = 'e';
+elif voiceInput == 'foxtrot':# hard to identify...
+    ch = 'f';
+elif voiceInput == 'golf':
+    ch = 'g';
+elif voiceInput == 'hotel':
+    ch = 'h';
+elif voiceInput == 'india':
+    ch = 'i';
+elif voiceInput == 'juliett':
+    ch = 'j';
+elif voiceInput == 'kilo':
+    ch = 'k';
+elif voiceInput == 'lima':
+    ch = 'l';
+elif voiceInput == 'mike':
+    ch = 'm';
+elif voiceInput == 'november':
+    ch = 'n';
+elif voiceInput == 'oscar':
+    ch = 'o';
+elif voiceInput == 'papa':
+    ch = 'p';
+elif voiceInput == 'quebec':
+    ch = 'q';
+elif voiceInput == 'romeo':
+    ch = 'r';
+elif voiceInput == 'sierra': #
+    ch = 's';
+elif voiceInput == 'tango': # confusing
+    ch = 't';
+elif voiceInput == 'uniform':
+    ch = 'u';
+elif voiceInput == 'victor':
+    ch = 'v';
+elif voiceInput == 'whiskey': #whisky
+    ch = 'w';
+elif voiceInput == 'xray':#x-ray
+    ch = 'x';
+elif voiceInput == 'yankee':
+    ch = 'y';
+elif voiceInput == 'zulu':#
+    ch = 'z';
+elif voiceInput == 'one':
+    ch = '1';
+elif voiceInput == 'two':#too
+    ch = '2';
+elif voiceInput == 'three':
+    ch = '3';
+elif voiceInput == 'four':#for
+    ch = '4';
+elif voiceInput == 'five':
+    ch = '5';
+elif voiceInput == 'six':
+    ch = '6';
+elif voiceInput == 'seven':
+    ch = '7';
+elif voiceInput == 'eight':
+    ch = '8';
+elif voiceInput == 'nine':
+    ch = '9';
+elif voiceInput == 'zero':
+    ch = '0';
+    
