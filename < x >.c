85ab8f6bbf1a9a9002e75db2060f66bbf2bfa61a
Hiroshi Ch <holdless2@gmail.com>
2016-03-03 13:28:09 +0800

    Update < x >.c

--- < x >.c
+++ < x >.c
@@ -3,63 +3,73 @@ _Bridge:
 ==========
   1. pi/air/win folder arrangement/github
   2. evernote tag
-  3. 大魚日和森林步道 咖啡
-  4. tokyo trip
+  //3. 大魚日和森林步道 咖啡
+  4. kyoto trip
   
 ==========
 TANGO:
 ==========
-----------
+[Voice Communication]
   Talk:
     1. *imperative and response
     2. *feedback following logics (p63)
     3. *conversation corpus
 ----------
-  Int:
+[Integration]
+  Setting:
     1. *(voice input) password input, wifi connect scenario
     //2. wifi connect (py config wifi)
     //3. *alphabet corresponding table (py)
-    4. battery status check (circuit included)
-    5. *survey: soft-hard lock mechanism
+  Diagnosis:
+    1. battery status check (circuit included)
+  Security:
+    1. *survey: soft-hard lock mechanism
 ----------
+[Vision]
   Facerec:
     1. *procedures: identifying face
-----------
   Tracking:
     1. *procidures: identifying tracking obj
 ----------
-  Remote:
-    1. *web-server
-      1) SRV: 
-        //a. *c call java webserver
-        //b. request for image -> response
-        //c. browser layout (shooting script)
-        d. login/password
-      2) client:
-        a. iOS: TCP socket
-        b. browser: 
-          1. *button location (css)
-          2. *button, input: <form action = "*.php"> (php), to server
-          3. *button, action, to server
-    //2. *OpenCV data info dispalying (shooting script)
-    3. live streaming
-      ////1. ffserver
-      2. mjpg-streamer
-      ////3. VLC
-      4. mjpg-streamer/web-server/opencv integration
+[Remote]
+  *Web-server:
+    1. SRV: 
+      //1) *c call java webserver
+      //2. request for image -> response
+      //3. browser layout (shooting script)
+      4. login/password
+    2. client:
+      1) iOS: TCP socket
+      2) browser: 
+        a. *button location (css)
+        b. *button, input: <form action = "*.php"> (php), to server
+        c. *button, action, to server
+    2. *OpenCV data info dispalying (shooting script & implement)
+  Live streaming:
+    ////1. ffserver
+    //2. mjpg-streamer
+    ////3. VLC
+    4. mjpg-streamer/web-server/opencv integration
 ----------
-  Mechanism & configuration:
+[Mechanism & configuration]
+  Head/neck:
     1. >head/neck mechanism & servos design
-    2. >ultrasonic module mechanism
-    3. >boards integrated mechanism on chasis
-    4. >other sensors
+  Configuration:
+    1. >boards integrated mechanism on chasis
+  Dynamic Sensor:
+    1. >ultrasonic module mechanism
+  Static Sensor:
+    1. >other sensors
 ----------
-  Navi:
+[Navi]
+  Sensor:
     1. ultrasonic function test
     2. motin sensor test
     3. *survey: laser distance
+  Algorithm:
+    1. SLAM
 ----------
-  DT:
+[Drivetrain]
     1. straight-moving problem
 ----------
 
@@ -67,25 +77,21 @@ TANGO:
 COLOMBO:
 ==========
 [Image Processing]
-----------
   Hot-zone detection:
     1) survey: IR cam / nightseen cam
     2) *algorithm
-----------
   Visual feature extraction:
     1) *algorithm
-----------
   Image stabilization:
     1) *image part
     2) video part
 ----------
-
 [Deep Learning]
-----------
   Tensorflow:
     1. tutorial
     2. adder/multiplier
-----------
+    3. ^RNN encoder/decoder paper
+    4. obj recognition using tensorflow
   ML:
     1. *ada-boost for cat face
     2. *word-embedding (hinton's assignment)
@@ -93,54 +99,51 @@ COLOMBO:
     4. ^read: generative vs. discriminative model
     5. *coursera: VC.dimension
 ----------
-
 [Robotics]
-----------
   SLAM:
     1. *coursera: algorithm study
+    2. ^MIT material
 ----------
-
 [Web technology]
-----------
   Searching:
     1. ^PageRank:
 ----------
-
 [Reverse Engineering]
-----------
   Fundamentals:
     1. *survey: if statement
-----------
 
 ==========
 WATT Lab:
 ==========
+  *Quadrotor: 
+    1. coursera
+    2. DIY with lego
 ----------
-  *Coursera: quadrotor
-----------
-  Drone DIY
-----------
-  Wall-e
-----------
+  Wall-e:
+    1. purchase interactive wall-e
+    2. transplant technology from "tango"
 
 ==========
 FEYNMAN Lab:
 ==========
-----------
+[Physics]
   ^Interstellar and related Einstein's theories
-----------
-  Informatin theory
-----------
   Quantum Mechanics
 ----------
+[C.S]
+  Informatin theory
+----------
+[Economics]
   *Behavioral Economics: paper submit
+
 ----------
   ^Books: 1) 精準預測 2) 17-eqs 3) 悖論 4) interstellar
+
 ==========
 IRON Lab:
 ==========
   Network Security Group:
-    1. Wifi hacking
+    1. Wifi hacking: vivek ramachandran's video lecture
 
 ==========
 MONET Studio:
