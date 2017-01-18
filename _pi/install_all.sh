2b30fba374556096938fa01ff5e1ffc4c1627689
Ihsorih <holdless2@gmail.com>
2017-01-19 07:03:24 +0800

    Update install_all.sh

--- _pi/install_all.sh
+++ _pi/install_all.sh
@@ -1,46 +1,49 @@
-###################
-## MANUALLY DOWNLOAD first!!!
-###################
-# <wiringPi>
+#---------------------------
+# MANUALLY DOWNLOAD first!!!
+#---------------------------
+# <Raspicam>
 #   (download file: http://www.uco.es/investiga/grupos/ava/node/40)
 
-###################
-### apt-get/pip ###
-###################
-### <RPi>
-sudo rpi-update (then reboot)
+#-----------
+# init RPi 
+#-----------
+sudo rpi-update
+#reboot
 # <RPi 2 -jessie: OpenGL error fix>
-sudo apt-get install libgl1-mesa-dri and then sudo reboot
-
-### <OpenCV>
-sudo apt-get install libopencv-dev python-opencv opencv-doc
-### <pi-camera: for OpenCV(python) to identify raspicam> 
-sudo apt-get install python-picamera
-### <python math-lib> 
-sudo apt-get install python-numpy
-### <cmake, i2c, telnet, kate> 
-sudo apt-get install cmake i2c-tools telnet kate
-### <audio>
-sudo apt-get install libasound2-dev
-# (setup sound car)
-alsamixer
-# (choose your usb sound card, and setup amp & mic volume)
-# for checking, enter:
-aplay -l
-# edit .asoundrc file
-sudo nano ~/.asoundrc
-# add: pcm.!default sysdefault:Device
-# save
-
-########
+#sudo apt-get install libgl1-mesa-dri
 reboot
-########
 
-# then use aplay <filename>
+#---------------------------------
+# <OpenCV-3.2.0 + contrib (dnn)>
+#---------------------------------
+#. install_opencv_dnn.sh
+
+#----------------
+# apt-get/pip 
+#----------------
+# <codeblocks>
+sudo apt-get install codeblocks
+
+# <tight vnc>
+# sudo apt-get install tightvncserver
+
+# <pi-camera: for OpenCV(python) to identify raspicam> 
+sudo apt-get install python-picamera
+
+# <python math-lib> 
+sudo apt-get install python-numpy
+
+# <i2c, telnet, kate> 
+sudo apt-get install i2c-tools telnet kate
+
+# <audio>
+sudo apt-get install libasound2-dev
 sudo apt-get install espeak bison python-dev swig               
-### <google speech API related>
+
+# <google speech API related>
 sudo apt-get install flac python-pycurl
-### <SpeechRecognition /python & related>
+
+# <SpeechRecognition /python & related>
 sudo apt-get install python-pyaudio
 sudo pip install SpeechRecognition 
 # (To quickly try it out, run python -m speech_recognition after installing)
@@ -48,42 +51,57 @@ sudo pip install SpeechRecognition
 # (this problem is due to in the new version of SpeechRecognition3.4.x, it requires new version of PyAudio (0.2.9), 
 # and if you only use: sudo pip pyaudio, it’ll tell you “Requirement already satisfied…”, so we need to add following “ --upgrade")
 sudo pip install pyaudio --upgrade
-### <pyttsx (espeak tts API) /python>
+
+# <pyttsx (espeak tts API) /python>
 sudo pip install pyttsx
-### <google translate tts API related>
+
+# <google translate tts API related>
 sudo apt-get install mplayer
-### <pyhton Wifi module>
+
+# <pyhton Wifi module>
 sudo pip install wifi
 
-### <node.js>
-sudo apt-get remove nodejs                
+reboot
 
-############
-### make ###
-############ 
-###### <h264 & ffmpeg>
-cd /usr/src
+#----------------------
+# download & install 
+#----------------------
+# <node.js>
+sudo apt-get remove nodejs
+cd /opt
+sudo wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+# you can test this by:
+#sudo node -v
+#sudo npm -v
+
+#-------
+# make 
+#------- 
+# <h264 & ffmpeg>
+cd /opt
 sudo git clone git://git.videolan.org/x264
 cd x264
 sudo ./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl
 sudo make
 sudo make install
 
-cd /usr/src
+cd /opt
 sudo git clone git://source.ffmpeg.org/ffmpeg.git
 cd ffmpeg
 sudo ./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree
 sudo make
 sudo make install
-### <wiringPi>
-cd /usr/src
-git clone git://git.drogon.net/wiringPi
+
+# <wiringPi>
+cd /opt
+sudo git clone git://git.drogon.net/wiringPi
 cd wiringPi
 sudo ./build
 
-##### <raspicam: for opencv(c++) recognise pi-cam>
+# <raspicam: for opencv(c++) recognise pi-cam>
 # (download file: http://www.uco.es/investiga/grupos/ava/node/40)
-unzip raspicam-0.1.3
+sudo unzip raspicam-0.1.3
 cd raspicam-0.1.3
 ##
 ## if MMAL RGB/BGR bug has been fixed (RPi after 2016/7 version)
@@ -100,15 +118,15 @@ sudo nano private_impl.cpp
 #  }
 # }
 ##
-mkdir build
+sudo mkdir build
 cd build
 sudo cmake ..
 sudo make
 sudo make install
 sudo ldconfig
 
-##### <mjpg-streamer>
-cd /usr/src/
+# <mjpg-streamer>
+cd /opt
 # download from git
 sudo git clone https://github.com/jacksonliam/mjpg-streamer.git
 #sudo apt-get install libv4l-dev imagemagick build-essential cmake subversion
@@ -139,3 +157,28 @@ sudo cp _build/plugins/output_http/output_http.so output_http.so
 #sudo cp _build/plugins/output_rtsp/output_rtsp.so output_rtsp.so
 #sudo cp _build/plugins/output_udp/output_udp.so output_udp.so
 #sudo cp _build/plugins/output_viewer/output_viewer.so output_viewer.so
+
+#@@@ software package/lib update @@@
+# 646.4.1110
+npm install socket.io #node.js
+
+reboot
+##################
+# modify setting #
+##################
+# <fixed IP setting>
+interface wlan0 # or etho for wired connection
+static ip_address=192.168.1.110
+static routers=192.168.1.1
+static domain_name_servers=192.168.1.1
+
+# <setup sound car>
+alsamixer
+# (choose your usb sound card, and setup amp & mic volume)
+# for checking, enter:
+aplay -l
+# edit .asoundrc file
+sudo nano ~/.asoundrc
+# add: pcm.!default sysdefault:Device
+# save
+# then use aplay <filename>
